170acff513fd5c6f24275a12b3b17441e1b194f0
DanielGermanosilva <ddger2018@gmail.com>
2023-01-31 21:51:30 -0300

    update class

--- Montadora/Models/Pneu.cs
+++ Montadora/Models/Pneu.cs
@@ -2,45 +2,45 @@
 {
     public class Pneu
     {
-        public string Cor { get; set; }
+        public string Cor { get; set; } = string.Empty;
         public int Aro { get; set; }
-        public string Tipo { get; set; }
+        public string Tipo { get; set; } = string.Empty;
         public bool TWI { get; set; }
         public int PERCENTUALBORRACHA { get; set; }
         public bool Estourado { get; set; }
-        public int velocidademaxima { get; set; }
-        public int velocidadeatual { get; set; }
-        public bool estepe { get; set; }
+        public int Velocidademaxima { get; set; }
+        public int Velocidadeatual { get; set; }
+        public bool Estepe { get; set; }
 
         public void Girar(int _velocidade)
         {
-            velocidadeatual = velocidadeatual + _velocidade;
+            Velocidadeatual = Velocidadeatual + _velocidade;
             PERCENTUALBORRACHA = PERCENTUALBORRACHA - 3;
             
-            if (velocidadeatual > velocidademaxima || PERCENTUALBORRACHA <=30)
+            if (Velocidadeatual > Velocidademaxima || PERCENTUALBORRACHA <=30)
             {
-                estourarpneu();
+                Estourarpneu();
             }
         }
 
-        public void estourarpneu()
+        public void Estourarpneu()
         {
             Estourado = true;
-            velocidadeatual = 0;
+            Velocidadeatual = 0;
         }
         public void Frear(int _reducao)
         {
-            velocidadeatual = velocidadeatual - _reducao;
+            Velocidadeatual = Velocidadeatual - _reducao;
             PERCENTUALBORRACHA = PERCENTUALBORRACHA - 5;
 
             if (PERCENTUALBORRACHA <=30)
             {
-                estourarpneu();
+                Estourarpneu();
             }
 
-            if (velocidadeatual < 0)
+            if (Velocidadeatual < 0)
             {
-                velocidadeatual = 0;
+                Velocidadeatual = 0;
             }
         }
 
