7138edb2474a640a3f362b5928e4494169136c57
DanielGermanosilva <ddger2018@gmail.com>
2023-02-03 21:32:34 -0300

    up

--- Montadora/ConsoleAppPrincipal/Program.cs
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -6,42 +6,17 @@ namespace ConsoleAppPrincipal
     {
         static void Main(string[] args)
         {
-            Pneu pneu1 = new Pneu();
-            pneu1.Aro = 16;
-            pneu1.PERCENTUALBORRACHA = 100;
-            pneu1.Cor = "Preto";
-            pneu1.Velocidademaxima = 150;
-            pneu1.Estepe = false;
-            pneu1.Estourado = false;
-            pneu1.Tipo = "Rua";
-            pneu1.TWI = false;
-            pneu1.Furado = false;
-            pneu1.Marca = "Michelin";
+           
 
-            Pneu pneu2 = new Pneu();
-            pneu2.Aro = 16;
-            pneu2.PERCENTUALBORRACHA = 100;
-            pneu2.Cor = "Preto";
-            pneu2.Velocidademaxima = 70;
-            pneu2.Estepe = true;
-            pneu2.Estourado = false;
-            pneu2.Tipo = "Rua";
-            pneu2.TWI = false;
-            pneu2.Furado = false;
-            pneu2.Marca = "Michelin";
+            
 
 
 
 
-            pneu1.Girar(20);
-            pneu1.Frear(1); ;
-            pneu1.Exibirinfopneu();
+            
 
-            Console.WriteLine("\n\n");
+            
 
-            pneu2.Girar(6);
-            pneu2.Frear(3); ;
-            pneu2.Exibirinfopneu();
         }
     }
 }
--- Montadora/Models/Carro.cs
+++ Montadora/Models/Carro.cs
@@ -1,12 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Models
 {
     internal class Carro
     {
+        public string? Fabricante { get; set; }  // ex. ford , audi etc
+        public int Potencia { get; set; } // potencia na roda 
+        public string? Cor { get; set; } // cor do veiculo
+        public string? Modelo { get; set; } // ex. onix, supra , gtr , polo
+        public string? Categoria { get; set; } // ex. esportivo , hypersportivo, offroad
+        public string? TipoCombustivel { get; set; } // flex, gasolina, alcool
+        public string? Tipo { get; set; } // sedan / conversivel
+        public int Odometro { get; set; } // 10000
+        public int Ano { get; set; } // 2022
+        public string? Placa { get; set; } // qgb 6652
+        public int VelocidadeMaximaCarro { get; set; } // 300 km/h
+        public bool CambioManual { get; set; } // true / false
+        public string? Qtd_Acentos { get; set; } // 2,5,7,9
+        public string? PesoCarro { get; set; } // 1000 kilos
+        public bool ArCondicionado { get; set; } // true / false
+        public int NivelCombustivel { get; set; } // 100
+        public bool CarroLigado { get; set; } // ligado bateria
+       
+
+        public Pneu PneuDianteiroEsquerdo { get; set; }
+        public Pneu PneuDianteiroDireito { get; set; }
+        public Pneu PneuTraseiroDireito { get; set; }
+        public Pneu PneuTraseiroEsquerdo { get; set; }
+        public Pneu PneuEstepe { get; set; }
+        public Motor Motor { get; set; }
+
+
+
+
+
+        public Carro(String _marca, string _Modelo, int _ano, int _velocidadeMaximaCarro, string _placa)
+        {
+            Fabricante = _marca;
+            Modelo = _Modelo;
+            Ano = _ano;
+            VelocidadeMaximaCarro = _velocidadeMaximaCarro;
+            Placa = _placa;
+            Odometro = 0;
+            CarroLigado = false;
+            NivelCombustivel = 0;
+
+            PneuDianteiroDireito = new Pneu(16, 150, "Carro Passeio");
+            PneuDianteiroEsquerdo = new Pneu(16, 150, "Carro Passeio");
+            PneuTraseiroDireito = new Pneu(16, 150, "Carro Passeio");
+            PneuTraseiroDireito = new Pneu(16, 150, "Carro Passeio");
+            PneuEstepe = new Pneu(16, 70, "Espete", true);
+            
+        }
+
+        public void LigarCarro()
+        {
+
+        }
+
+        public void Acelerar()
+        {
+
+        }
+
+        public void Frear()
+        {
+
+
+        }
+
+
+        public void Abastecer()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
